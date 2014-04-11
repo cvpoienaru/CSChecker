76c83e97e48de8eb29bc8c2708fdad9c89429cb9
cvpoienaru <cvpoienaru@gmail.com>
2014-04-11 16:37:21 +0300

    License modified

--- CSChecker/Definitions/TestResult.cs
+++ CSChecker/Definitions/TestResult.cs
@@ -8,26 +8,26 @@
  ***		Copyright (c) 2014, Codrin-Victor Poienaru.											***
  ***		All rights reserved.																***
  ***																							***
- ***	Redistribution and use in source and binary forms, with or without						***
- ***	modification, are permitted provided that the following conditions are met:				***
+ ***	Redistribution and use in source and binary forms, with or without modification,		***
+ ***	are permitted provided that the following conditions are met:							***
  ***																							***
- ***		* Redistributions of source code must retain the above copyright notice, this		***
- ***		list of conditions and the following disclaimer.									***
+ ***		* Redistributions of source code must retain the above copyright notice, this list	***
+ ***		of conditions and the following disclaimer.											***
  ***																							***
- ***		* Redistributions in binary form must reproduce the above copyright notice,			***
- ***		this list of conditions and the following disclaimer in the documentation			***
- ***		and/or other materials provided with the distribution.								***
+ ***		* Redistributions in binary form must reproduce the above copyright notice, this	***
+ ***		list of conditions and the following disclaimer in the documentation and/or other	***
+ ***		materials provided with the distribution.											***
  ***																							***
- ***	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"				***
- ***	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE				***
- ***	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE			***
- ***	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE			***
- ***	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL				***
- ***	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR				***
- ***	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER				***
- ***	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,			***
- ***	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE			***
- ***	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.					***
+ ***	This software is provided by the copyright holders and contributors "as is" and any		***
+ ***	express or implied warranties, including, but not limited to, the implied warranties	***
+ ***	of merchantability and fitness for a particular purpose are disclaimed. In no event		***
+ ***	shall the copyright holder or contributors be liable for any direct, indirect,			***
+ ***	incidental, special, exemplary, or consequential damages (including, but not limited	***
+ ***	to, procurement of substitute goods or services; loss of use, data, or profits; or		***
+ ***	business interruption) however caused and on any theory of liability, whether in		***
+ ***	contract, strict liability, or tort (including negligence or otherwise) arising in		***
+ ***	any way out of the use of this software, even if advised of the possibility of such		***
+ ***	damage.																					***
  ***																							***
  **************************************************************************************************
  **************************************************************************************************
@@ -41,9 +41,19 @@ using System.Threading.Tasks;
 
 namespace CSChecker.Definitions
 {
+	/// <summary>
+	/// Specifies values representing the possible results of a test evaluation.
+	/// </summary>
 	public enum TestResult
 	{
+		/// <summary>
+		/// Indicates that a test failed.
+		/// </summary>
 		Failed = 0,
+
+		/// <summary>
+		/// Indicates that a test passed.
+		/// </summary>
 		Passed = 1
 	}
 }
